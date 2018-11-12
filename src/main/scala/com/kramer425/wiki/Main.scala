37dbcbad5c2a82658a1b261600badf4f0cd294c7
Dan Kramer <kramer425@gmail.com>
2018-11-12 18:06:04 -0500

    added stanford parser lexer

--- src/main/scala/com/kramer425/wiki/Main.scala
+++ src/main/scala/com/kramer425/wiki/Main.scala
@@ -2,7 +2,7 @@ package com.kramer425.wiki
 
 import com.kramer425.wiki.config.WikiConfig
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{col, concat_ws}
 
 object Main {
 
@@ -26,17 +26,12 @@ object Main {
 
     pagesDs.cache()
 
-    //    val tfidfProcessor = TFIDFProcessing(spark)
-    //
-    //    val (columns, tfidf) = tfidfProcessor.tfidf(pagesDs)
-    //
-    //    tfidf.show(5)
-    //    tfidf.printSchema()
-
-    pagesDs.write
-      .format(wikiConfig.outputFormat)
-      .option("delimiter", wikiConfig.outputDelimiter)
-      .save(wikiConfig.outputDir)
+    if(wikiConfig.outputRegularPath.isDefined){
+      pagesDs.write
+        .format(wikiConfig.outputFormat)
+        .option("delimiter", wikiConfig.outputDelimiter)
+        .save(wikiConfig.outputRegularPath.get)
+    }
 
     println(s"Articles processed: ${pagesDs.count()}")
 
@@ -51,6 +46,37 @@ object Main {
       .describe()
       .show()
 
+    val preprocesser = Preprocessing(spark)
+    val lemmatizedPagesDs = preprocesser.lemmatizeWikiPages(pagesDs)
+    lemmatizedPagesDs.cache()
+
+    if(wikiConfig.outputLemmatizedPath.isDefined){
+      lemmatizedPagesDs.select(
+        col("id"),
+        col("title"),
+        concat_ws(" ", col("summary")),
+        concat_ws(" ", col("body")))
+        .write
+        .format(wikiConfig.outputFormat)
+        .option("delimiter", wikiConfig.outputDelimiter)
+        .save(wikiConfig.outputLemmatizedPath.get)
+    }
+
+    lemmatizedPagesDs.map(lemmatizedWikiPage => Array(lemmatizedWikiPage.summary.length, lemmatizedWikiPage.body.length))
+      .select(
+        col("value").getItem(0).as("lemmatizedSummaryLength"),
+        col("value").getItem(1).as("lemmatizedBodyLength")
+      )
+      .describe()
+      .show()
+
+    if(wikiConfig.outputVocabularyPath.isDefined){
+      val vocabBuilder = VocabBuilder(spark)
+      val vocabDs = vocabBuilder.vocabulary(lemmatizedPagesDs)
+      vocabDs.repartition(1).write.text(wikiConfig.outputVocabularyPath.get)
+    }
+
+    lemmatizedPagesDs.unpersist()
     pagesDs.unpersist()
 
     val endTime = System.nanoTime()
---
+++ src/main/scala/com/kramer425/wiki/Preprocessing.scala
@@ -0,0 +1,99 @@
+package com.kramer425.wiki
+
+import org.apache.spark.sql.{Dataset, SparkSession}
+import java.text.Normalizer
+import java.text.Normalizer.Form
+import java.util.Properties
+
+import edu.stanford.nlp.ling.CoreAnnotations.{LemmaAnnotation, SentencesAnnotation, TokensAnnotation}
+import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
+import org.apache.spark.broadcast.Broadcast
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
+
+case class LemmatizedWikiPage(id: Long, title: String, summary: Seq[String], body: Seq[String])
+
+class Preprocessing(spark: SparkSession) extends Serializable {
+  import spark.implicits._
+
+  val NUMBER_TOKEN = "<NUMBER>"
+
+  // Taken from: https://rosettacode.org/wiki/Determine_if_a_string_is_numeric#Scala
+  val numberRegex = """[+-]?((\d+(e\d+)?[lL]?)|(((\d+(\.\d*)?)|(\.\d+))(e\d+)?[fF]?))""".r
+
+  private val bStopWords: Broadcast[Set[String]] = loadStopWords()
+
+  private def loadStopWords(): Broadcast[Set[String]] = {
+    val is = getClass.getResourceAsStream("/org/apache/spark/ml/feature/stopwords/english.txt")
+    val stopWords = scala.io.Source.fromInputStream(is)(scala.io.Codec.UTF8).getLines().toSet
+    spark.sparkContext.broadcast(stopWords)
+  }
+
+  // Lowercases, converts unicode to ascii. Some characters like Mandarin characters are left in
+  // Modified from: https://gist.github.com/agemooij/15a0eaebc2c1ddd5ddf4
+  private def normalizeString(str: String): String = {
+    val cleaned = str.trim.toLowerCase
+    val normalized = Normalizer.normalize(cleaned, Form.NFD).replaceAll("[\\p{InCombiningDiacriticalMarks}\\p{IsM}\\p{IsLm}\\p{IsSk}]+", "")
+
+    normalized.replaceAll("'s", "")
+      .replaceAll("ß", "ss")
+      .replaceAll("ø", "o")
+      .replaceAll("₣", "f")
+      .replaceAll("₳", "a")
+      .replaceAll("㎝", "cm")
+      .replaceAll("‒", "-")
+  }
+
+  def lemmatizeWikiPages(wikiPages: Dataset[WikiPage]): Dataset[LemmatizedWikiPage] = {
+    wikiPages.mapPartitions(iter => {
+      val stanfordNLP = nlpInstance()
+      iter.map(wikiPage => lemmatizeWikiPage(wikiPage, stanfordNLP, bStopWords.value))
+    })
+  }
+
+  private def lemmatizeWikiPage(wikiPage: WikiPage, stanfordNLP: StanfordCoreNLP, stopWords: Set[String]): LemmatizedWikiPage = {
+    val nSummary = normalizeString(wikiPage.summary)
+    val nBody = normalizeString(wikiPage.body)
+
+    val summaryLemmas: Seq[String] = plainTextToLemmas(nSummary, stanfordNLP, stopWords)
+    val bodyLemmas : Seq[String] = plainTextToLemmas(nBody, stanfordNLP, stopWords)
+
+    LemmatizedWikiPage(wikiPage.id, wikiPage.title, summaryLemmas, bodyLemmas)
+  }
+
+  private def nlpInstance(): StanfordCoreNLP = {
+    val props = new Properties()
+    props.put("annotators", "tokenize, ssplit, pos, lemma")
+    new StanfordCoreNLP(props)
+  }
+
+  private def isOnlyLetters(str: String): Boolean = {
+    str.forall(c => Character.isLetter(c))
+  }
+
+  private def isNumber(str: String): Boolean = {
+    numberRegex.pattern.matcher(str).matches()
+  }
+
+  // Convert text to Seq of lemmas. Add sentence start tokens and sentence end tokens. Replace numbers with token
+  private def plainTextToLemmas(text: String, stanfordNLP: StanfordCoreNLP, stopWords: Set[String]): Seq[String] = {
+    val doc = new Annotation(text)
+    stanfordNLP.annotate(doc)
+    val lemmas = new ArrayBuffer[String]()
+    val sentences = doc.get(classOf[SentencesAnnotation])
+    for (sentence <- sentences.asScala; token <- sentence.get(classOf[TokensAnnotation]).asScala) {
+        val lemma = token.get(classOf[LemmaAnnotation]).toLowerCase
+        if (lemma.length > 2 && !stopWords.contains(lemma) && isOnlyLetters(lemma)) {
+          lemmas += lemma
+        }else if (isNumber(lemma)) {
+          lemmas += NUMBER_TOKEN
+        }
+    }
+    lemmas
+  }
+}
+
+object Preprocessing {
+  def apply(spark: SparkSession): Preprocessing = new Preprocessing(spark)
+}
